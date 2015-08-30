2942aa359efbbfe178604a9711e082f351474b79
Micah Alcorn <admin@micahalcorn.com>
2015-08-30 15:35:05 -0500

    Initial commit

---
+++ package.js
@@ -0,0 +1,23 @@
+Package.describe({
+  name: 'micah:google-cloud',
+  version: '0.0.1',
+  summary: 'Wrapper for Node "gcloud" package',
+  git: '',
+  documentation: 'README.md'
+});
+
+Package.onUse(function (api) {
+  api.versionsFrom('1.1.0.3');
+  api.addFiles('lib/google-cloud.js', 'server');
+  api.export('gcloud', 'server');
+});
+
+Package.onTest(function (api) {
+  api.use('tinytest');
+  api.use('micah:google-cloud');
+  api.addFiles('lib/google-cloud-tests.js', 'server');
+});
+
+Npm.depends({
+  'gcloud': '0.20.0'
+});
